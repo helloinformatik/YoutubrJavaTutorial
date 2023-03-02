de253c923facadfbfe4ebe49f5272b8b3810860c
helloinformatik <wi10004ok@gmail.com>
2023-03-02 22:39:36 +0100

    how to create a game board

--- src/JavaLessonEight.java
+++ src/JavaLessonEight.java
@@ -1,2 +1,9 @@
+import java.util.Arrays;
+import org.apache.commons.lang3.ArrayUtils;
+
 public class JavaLessonEight {
+    public static void main (String[] args){
+        MonsterTwo.buildBattleBoard();
+        char[][] tempBattleBoard = new char [10][10];
+    }
 }
--- src/MonsterTwo.java
+++ src/MonsterTwo.java
@@ -6,14 +6,33 @@ public class MonsterTwo {
             Arrays.fill(row, '*');
         }
     }
+    public static void redrawBoard() {
+        int k = 1;
+        while (k <= 30) { System.out.print('-'); k++;}
+        System.out.println();
+        for(int i = 0; i < battleBoard.length; i++){
+            for(int j =0; j< battleBoard[i].length;j++){
+                System.out.print("|" + battleBoard[i][j] + "|");
+            }
+            System.out.println();
+        }
+        k = 1;
+        while (k <= 30) { System.out.print('-'); k++;}
+        System.out.println();
+
+    }
+
     public final String TOMBSTONE = " Here Lies a Dead monster";
     private int health = 500;
     private int attack = 20;
     private int movement = 2;
+
+    private boolean alive = true;
+    public String name = " Big Monster";
+    public char nameChar1 = 'B';
     private int xPosition = 0;
     private int yPosition = 0;
-    private boolean alive = true;
-    public String name = " Big Monster ";
+    public static int numOfMonsters = 0;
 
     public int getAttack(){
         return attack;
@@ -38,13 +57,28 @@ public class MonsterTwo {
         }
     }
 
-    public MonsterTwo(int health, int attack, int movement){
+    public MonsterTwo(int health, int attack, int movement, String name){
         this.health = health;
         this.attack = attack;
         this.movement = movement;
+        this.name = name;
+        int maxXBoardSpace = battleBoard.length -1;
+        int maxYBoardSpace = battleBoard[0].length -1;
+
+        int randNumX, randNumY;
+
+        do {
+            randNumX=(int) (Math.random() * maxXBoardSpace);
+            randNumY=(int) (Math.random() * maxYBoardSpace);
+        } while(battleBoard[randNumX][randNumY] != '*');
+        this.xPosition=randNumX;
+        this.yPosition=randNumY;
+        this.nameChar1=this.name.charAt(0);
+        battleBoard[this.yPosition][this.xPosition] =this.nameChar1;
+        numOfMonsters++;
     }
     public MonsterTwo(){
-
+        numOfMonsters++;
     }
     public static void main(String[] args){
         MonsterTwo Frank = new MonsterTwo();
